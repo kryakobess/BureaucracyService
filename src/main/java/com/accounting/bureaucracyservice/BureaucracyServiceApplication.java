08f59dc2f08fcae24a3516d55908fb8de4873649
Nikita Kuritsyn <martyz89@yandex.ru>
2024-01-08 14:55:11 +0300

    Initialized database

---
+++ src/main/java/com/accounting/bureaucracyservice/BureaucracyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.accounting.bureaucracyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BureaucracyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BureaucracyServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/accounting/bureaucracyservice/BureaucracyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accounting.bureaucracyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BureaucracyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
